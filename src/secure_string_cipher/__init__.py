0e88e2922bcc9234c50bcd2cb25a88fa96269d52
TheRedTower <security@avondenecloud.uk>
2025-12-02 01:22:58 +0000

    refactor: improve public API organization in __init__.py
    - Add SecurityError to public exports for external use
    - Organize __all__ with categorical comments (Encryption, Exceptions,
      Security utilities, Secure memory, Passphrase management, CLI utilities)
    - All 210 tests pass, pre-commit checks pass

--- src/secure_string_cipher/__init__.py
+++ src/secure_string_cipher/__init__.py
@@ -17,6 +17,7 @@ from .core import (
 from .passphrase_generator import generate_passphrase
 from .passphrase_manager import PassphraseVault
 from .secure_memory import SecureBytes, SecureString, secure_wipe
+from .security import SecurityError
 from .timing_safe import (
     add_timing_jitter,
     check_password_strength,
@@ -24,11 +25,12 @@ from .timing_safe import (
 )
 from .utils import ProgressBar, colorize, handle_timeout, secure_overwrite
 
-__version__ = "1.0.17"
+__version__ = "1.0.18"
 __author__ = "TheRedTower"
 __email__ = "security@avondenecloud.uk"
 
 __all__ = [
+    # Encryption
     "encrypt_text",
     "decrypt_text",
     "encrypt_file",
@@ -36,16 +38,22 @@ __all__ = [
     "encrypt_stream",
     "decrypt_stream",
     "derive_key",
-    "CryptoError",
     "StreamProcessor",
+    # Exceptions
+    "CryptoError",
+    "SecurityError",
+    # Security utilities
     "check_password_strength",
     "constant_time_compare",
     "add_timing_jitter",
+    # Secure memory
     "SecureString",
     "SecureBytes",
     "secure_wipe",
+    # Passphrase management
     "generate_passphrase",
     "PassphraseVault",
+    # CLI utilities
     "colorize",
     "handle_timeout",
     "secure_overwrite",
